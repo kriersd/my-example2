c9502ac86343923d0d7db2327edb1321ef355a10
David Krier <dkflyboy@gmail.com>
2019-12-20 16:58:46 -0600

    my first changes

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.render('index', {msg: "Hello Express!!!"});
+    res.render('index', {msg: "Hello Express!!! 2"});
 });
 
 module.exports = router;
